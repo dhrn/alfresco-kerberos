a3e94fdf7b5470428386c0cb1397ce28ada08192
dhrn <dharan.g@muraai.com>
2021-09-30 01:47:10 +0530

    * fix docs

--- start.sh
+++ start.sh
@@ -4,7 +4,6 @@ printf "\n======== stop the running env ========\n\n"
 
 rm -rf .keytabs && mkdir .keytabs;
 docker-compose down -v;
-lsof -i:88 && lsof -i:464 && lsof -i:749;
 sleep 20;
 
 printf "\n======== build and run docker images ========\n\n"
